8c2c4716e0b6cb0aa420496fbbc68a1333de7379
Tejal Shanbhag <tejalvs14@gmail.com>
2020-10-25 13:35:04 -0400

    updated version 2

--- hw08_part2.py
+++ hw08_part2.py
@@ -6,21 +6,19 @@
 import pandas as pd
 
 def read_crossCorr():
-    data = pd.read_csv('Cross-Correlation Result.csv')
+    data = pd.read_csv('HW_PCA_SHOPPING_CART_v896.csv')
     columns=list(data.columns[1:])
     rows=list(data.iloc[:, 0])
     return data,rows,columns
 
-def findMin(data,rows,col):
-    for row_values in range(0,len(rows)):
-        temp_row=list(data.iloc[row_values])
-        min_row_value=min(temp_row[1:])
-        min_row_idx=temp_row.index(min_row_value)
-        print(min_row_value,min_row_idx,col[min_row_idx])
-
+def generateDistanceMatrix(data,rows,coloumns):
+    for i in range(0, len(rows)):
+        row1=data.loc[i]
+        row2=data.loc[i+1]
+        print()
 
 
 
 
 data,rows,cols=read_crossCorr()
-findMin(data,rows,cols)
+generateDistanceMatrix(data,rows,cols)
